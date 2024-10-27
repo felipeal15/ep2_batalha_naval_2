b12c4bc46c7108aeb5d35c1e02f9b44529072d3b
felipeal15 <felipeal3@al.insper.edu.br>
2024-10-26 21:01:12 -0300

    Exercicio 2
    (a função preenche_frota sendo implementada)

--- funcoes.py
+++ funcoes.py
@@ -6,3 +6,12 @@ def define_posicoes(linha, coluna, ori, tamanho):
         elif ori == "horizontal":
             posicoes.append([linha, coluna + i])
     return posicoes
+
+
+def preenche_frota(frota, navio, linha, coluna, ori, tamanho):
+    posicao = define_posicoes(linha, coluna, ori, tamanho)
+    if navio not in frota:
+        frota[navio] = []
+    frota[navio].append(posicao)
+    
+    return frota
